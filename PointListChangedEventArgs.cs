cdcef6f3e0aa4141084c15dedf82628a4493ed13
bblakistone <btb@abacuslaw.com>
2017-04-14 14:57:09 -0700

    new class

---
+++ PointListChangedEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3_1
+{
+    public class PointListChangedEventArgs : EventArgs
+    {
+        public enum PointListChangedOperations{Unknown, Add, Remove, Insert, Clear, Update}
+
+
+    }
+}
