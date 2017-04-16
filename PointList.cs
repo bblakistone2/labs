d742efcd35404df6dc05dffb547839eb40a13df7
Brian <bblakistone@gmail.com>
2017-04-15 20:38:14 -0700

    Getting there...

--- PointList.cs
+++ PointList.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,97 @@ using System.Threading.Tasks;
 
 namespace lab3_1
 {
-    class PointList
+    public class PointList : IList<Point>
     {
-        private List<Point> _Points = new List<Point>();
+        private List<Point> _Points = new List<Point>();
+
+        public Point this[int index]
+        {
+            get
+            {
+                return _Points[index];
+            }
+
+            set
+            {
+                _Points[index] = value;
+                Changed?.Invoke(this, new PointListChangedEventArgs(PointListChangedEventArgs.PointListChangedOperations.Update));
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return  _Points.Count;
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                return ((ICollection<Point>)_Points).IsReadOnly;
+
+            }
+        }
+
+        public event EventHandler<PointListChangedEventArgs> Changed;
+
+        public void Add(Point item)
+        {
+             _Points.Add(item);
+            Changed?.Invoke(this, new PointListChangedEventArgs(PointListChangedEventArgs.PointListChangedOperations.Add));
+        }
+
+        public void Clear()
+        {
+            _Points.Clear();
+                Changed?.Invoke(this, new PointListChangedEventArgs(PointListChangedEventArgs.PointListChangedOperations.Clear));
+        }
+
+        public bool Contains(Point item)
+        {
+            return  _Points.Contains(item);
+        }
+
+        public void CopyTo(Point[] array, int arrayIndex)
+        {
+            _Points.CopyTo(array, arrayIndex);
+        }
+
+        public IEnumerator<Point> GetEnumerator()
+        {
+            return  _Points.GetEnumerator();
+        }
+
+        public int IndexOf(Point item)
+        {
+            return _Points.IndexOf(item);
+        }
+
+        public void Insert(int index, Point item)
+        {
+            _Points.Insert(index, item);
+            Changed?.Invoke(this, new PointListChangedEventArgs(PointListChangedEventArgs.PointListChangedOperations.Insert));
+        }
+
+        public bool Remove(Point item)
+        {
+            return _Points.Remove(item);
+            Changed?.Invoke(this, new PointListChangedEventArgs(PointListChangedEventArgs.PointListChangedOperations.Remove));
+        }
+
+        public void RemoveAt(int index)
+        {
+            _Points.RemoveAt(index);
+            Changed?.Invoke(this, new PointListChangedEventArgs(PointListChangedEventArgs.PointListChangedOperations.Remove));
+
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return  _Points.GetEnumerator();
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,8 +8,27 @@ namespace lab3_1
 {
     class Program
     {
+        static PointList points = new PointList();
         static void Main(string[] args)
         {
+            points.Changed += delegate (object sender, PointListChangedEventArgs e)
+            {
+                Console.WriteLine($"Change happened: {e.Operation}");
+            };
+
+            points.Add(new Point(-4, -7));
+            points.Add(new Point(0, 0));
+            points.Add(new Point(1, 2));
+            points.Add(new Point(-4, 5));
+            points.Insert(2, new Point(3, 1));
+            points.Add(new Point(7, -2));
+            points[0] = new Point(2, 1);
+            points.RemoveAt(2);            Console.WriteLine();
+            if (points.Any(points => points.X == 0 && points.Y == 0))
+            {
+                Console.WriteLine("There is a point at the origin in this set");
+                    };
+               
 
             Console.WriteLine("Press <ENTER> to exit...");
             Console.ReadLine();
