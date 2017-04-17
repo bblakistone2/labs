04e344ba83688ebe0a153aba5fda3ac4689e8635
bblakistone <btb@abacuslaw.com>
2017-04-17 13:28:43 -0700

    2nd delegate

--- Program.cs
+++ Program.cs
@@ -23,15 +23,28 @@ namespace lab3_1
             points.Insert(2, new Point(3, 1));
             points.Add(new Point(7, -2));
             points[0] = new Point(2, 1);
-            points.RemoveAt(2);            Console.WriteLine();
+            points.RemoveAt(2);
+
+            Console.WriteLine();
+
             if (points.Any(points => points.X == 0 && points.Y == 0))
             {
                 Console.WriteLine("There is a point at the origin in this set");
                     };
-               
+
+            Console.WriteLine();
+            Console.WriteLine("Points from this set in Quadrant I:");
+            foreach (Point p in points.Where(points => points.X > 0 && points.Y>0))
+            {
+                Console.WriteLine(p);
+            }
+            if (points.All(points => points.X < 20)) {Console.WriteLine("All points have X less than 20");
+                    };
+           
 
             Console.WriteLine("Press <ENTER> to exit...");
-            Console.ReadLine();
+            Console.ReadLine();
+
         }
     }
 }
